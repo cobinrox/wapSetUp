21db7c8874821b39596f2bb8844c144cbe252a79
CJ Rox <mescgroup@yahoo.com>
2015-12-28 07:45:52 -0700

    Freeplane mindmap

---
+++ wapSetUp_freplane_mindmap.mm
@@ -0,0 +1,82 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="wapSetUp" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1451313404667"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="SOFTWARE" POSITION="right" ID="ID_696475516" CREATED="1451313409895" MODIFIED="1451313419475">
+<edge COLOR="#ff0000"/>
+<node TEXT="Custom Scripts" ID="ID_838239879" CREATED="1451313429590" MODIFIED="1451313435778">
+<node TEXT="install scripts" ID="ID_1316327917" CREATED="1451313508810" MODIFIED="1451313521289"/>
+<node TEXT="disable script" ID="ID_342574546" CREATED="1451313523507" MODIFIED="1451313529538"/>
+<node TEXT="re-enable script" ID="ID_1759689707" CREATED="1451313533458" MODIFIED="1451313538162"/>
+</node>
+<node TEXT="Third Party Software" ID="ID_948683679" CREATED="1451313437976" MODIFIED="1451313446048">
+<node TEXT="hostapd" ID="ID_769214809" CREATED="1451313552761" MODIFIED="1451313868213">
+<node TEXT="adafruit hostapad_14128" ID="ID_1238756099" CREATED="1451313841078" MODIFIED="1451313868212" VSHIFT="10"/>
+</node>
+<node TEXT="isc-dhcp-server" ID="ID_464931874" CREATED="1451313560876" MODIFIED="1451313584718"/>
+</node>
+<node TEXT="O/S Software" ID="ID_1415202769" CREATED="1451313447611" MODIFIED="1451313454167">
+<node TEXT="/etc/network" ID="ID_1630934694" CREATED="1451313539291" MODIFIED="1451313597760"/>
+<node TEXT="update-rc.d" ID="ID_1277149123" CREATED="1451313588431" MODIFIED="1451313631605"/>
+</node>
+</node>
+<node TEXT="HARDWARE" POSITION="right" ID="ID_967636154" CREATED="1451313423492" MODIFIED="1451313427550">
+<edge COLOR="#0000ff"/>
+<node TEXT="Raspi" ID="ID_1785710237" CREATED="1451313456256" MODIFIED="1451313637880"/>
+<node TEXT="Edimax Wireless Adapter" ID="ID_900847042" CREATED="1451313460778" MODIFIED="1451313646049"/>
+<node TEXT="Secondary Login Capability" ID="ID_389157723" CREATED="1451313469843" MODIFIED="1451313655349">
+<node TEXT="serial console cable" ID="ID_1371386010" CREATED="1451313489063" MODIFIED="1451313494376"/>
+<node TEXT="hard ethernet cable" ID="ID_1077482071" CREATED="1451313496226" MODIFIED="1451313503938"/>
+</node>
+<node TEXT="Internet access (for initial installation)" ID="ID_445492245" CREATED="1451313707279" MODIFIED="1451313722035"/>
+</node>
+</node>
+</map>
